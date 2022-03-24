5e355a0bc4c640d4442fc2158e796a4ff130c3ca
marvinraven <marvin@raven.com>
2022-03-24 12:05:18 -0600

    Added files to repo

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { MenuController } from 'ionic-angular';
 
 @Component({
   selector: 'page-hello-ionic',
   templateUrl: 'hello-ionic.html'
 })
 export class HelloIonicPage {
-  constructor() {
+  fetchInterval: any;
 
+
+  constructor(public menuCtrl: MenuController) {}
+
+  spamMenu(): void {
+    let countSpam = 0;
+    this.fetchInterval = setInterval(async () => {
+      if (countSpam < 500) {
+        this.menuCtrl.isOpen() ? this.menuCtrl.close() : this.menuCtrl.open();
+      } else {
+        this.fetchInterval = undefined;
+      }
+      countSpam++;
+    }, 200);
   }
 }
